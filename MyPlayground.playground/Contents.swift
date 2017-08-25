0d44f478d806a07911b6da3e4234983efc2a6025
gotech <gotech@tomdeimac.local>
2017-08-25 14:39:21 +0800

    initial file for swift 3

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit // User interface elements
+
+// Variables
+// var is editable
+// let is final
+var str: String = "Hello, playground"
+str = "ryan"
+
+var num: Int = 32767
+
+var dou: Double = 15.16984
+
+var flo: Float = 22.44
+
+var bool: Bool = true
+
+
+// ------------- Elements of Collections ----------------
+// Arrays
+// not unique
+// unordered
+
+var arrayInts: [Int] = [66,1522]
+var arrayString = [String] ()
+
+arrayInts.append(5)
+arrayInts.append(32455)
+
+arrayInts += [1,2,3]
+arrayInts.sort()
+
+print(arrayInts.count)
+print(arrayString.count)
+
+print(arrayInts[2])
+
+
+// Sets
+// unordered
+// unique
+var setChars = Set<Character>()
+
+setChars.insert("a")
+adsadada
+var setString: Set<String> = ["ryan", "kristoffer", "ryan", "joenna"]
